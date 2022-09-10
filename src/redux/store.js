60708edb0cd70c12d13b8e8c25b5bd712d7d48ba
AnnaNesterenko <nura_arh@ukr.net>
2022-09-10 10:17:11 +0300

    delPersist

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contacts-slice';
 import filterReducer from './filter-slice';
 
-// const rootReducer = combineReducers({
-//   contacts: contactsReducer,
-//   filter: filterReducer,
-// });
-
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,6 @@ const instance = axios.create({
 
 export const getContacts = async () => {
   const { data } = await instance.get('/');
-
   return data;
 };
 
