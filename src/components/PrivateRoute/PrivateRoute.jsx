80410fd463452e5109ba4c48083bbf5a0bf08fa3
AnnaNesterenko <nura_arh@ukr.net>
2022-09-19 15:27:45 +0300

    delForBuild

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,13 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { isAuth } from 'redux/auth/authSelectors';
 
 export const PrivateRoute = ({ children }) => {
-    const isLogin = useSelector(isAuth);
+  const isLogin = useSelector(isAuth);
 
-    if (!isLogin) {
-      return <Navigate to="/login" />;
-    }
+  if (!isLogin) {
+    return <Navigate to="/login" />;
+  }
   //   return <Outlet />;
   return children;
 };
